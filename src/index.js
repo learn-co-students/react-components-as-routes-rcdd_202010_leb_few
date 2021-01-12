b693cb4c757ab2f4772e4f2b8baa1491a87d2f7e
Jana Khanji <janokhanji@gmail.com>
2021-01-12 06:44:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -17,6 +17,24 @@ const About = () => {
     </div>
   );
 };
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 ReactDOM.render((
   <Router>
     <Route path="/" component={Home} />
